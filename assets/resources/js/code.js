359397bc344ed5f726ed61325035e3e8eb50a38a
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-11 22:10:36 -0600

    Better responsive, no more alerts and re captcha form inactive

--- assets/resources/js/code.js
+++
@@ -1,6 +0,0 @@
-alert(
-  "Hola, Bienvenido a mi portafolio web,Recuerda que este portafolio sigue siendo actualizado hasta la fecha!!!"
-);
-console.log(
-  "¿Que haces aqui? Espero que ya hayas explorado el contenido del portafolio"
-);
--- assets/resources/js/validacion.js
+++ assets/resources/js/validacion.js
@@ -1,17 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("myForm");
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const subjectInput = document.getElementById("subject");
+  const bodyInput = document.getElementById("body");
   const submitButton = document.getElementById("submit");
 
-  form.addEventListener("submit", function (event) {
-    // Validar campos aquí
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const subject = document.getElementById("subject").value;
-    const body = document.getElementById("body").value;
+  // Función para validar el campo nombre en tiempo real
+  nameInput.addEventListener("input", function () {
+    if (nameInput.value.trim() === "") {
+      nameInput.setCustomValidity("Por favor, completa este campo.");
+    } else {
+      nameInput.setCustomValidity("");
+    }
+  });
 
-    if (!name || !email || !subject || !body) {
-      alert("Por favor, completa todos los campos.");
-      event.preventDefault(); // Detener el envío del formulario si hay campos vacíos
+  // Función para validar el campo email en tiempo real
+  emailInput.addEventListener("input", function () {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(emailInput.value)) {
+      emailInput.setCustomValidity("Por favor, ingresa una dirección de correo válida.");
+    } else {
+      emailInput.setCustomValidity("");
+    }
+  });
+
+  // Función para validar el campo asunto en tiempo real
+  subjectInput.addEventListener("input", function () {
+    if (subjectInput.value.trim() === "") {
+      subjectInput.setCustomValidity("Por favor, completa este campo.");
+    } else {
+      subjectInput.setCustomValidity("");
+    }
+  });
+
+  // Función para validar el campo mensaje en tiempo real
+  bodyInput.addEventListener("input", function () {
+    if (bodyInput.value.trim() === "") {
+      bodyInput.setCustomValidity("Por favor, completa este campo.");
+    } else {
+      bodyInput.setCustomValidity("");
+    }
+  });
+
+  // Agregar evento de escucha para validar el formulario al enviar
+  form.addEventListener("submit", function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault(); // Detener el envío del formulario si hay campos no válidos
     }
   });
 });
